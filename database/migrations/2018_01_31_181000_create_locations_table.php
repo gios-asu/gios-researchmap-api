106373721da6561cb1524083881af1e36e05aa94
Nathan Rollins <nathan.rollins@asu.edu>
2018-01-31 11:14:22 -0700

    Commit database migrations

---
+++ database/migrations/2018_01_31_181000_create_locations_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->string('id', 3)->unique();
+            $table->string('name', 255);
+            $table->integer('size');
+            $table->float('latitude', 13, 10);
+            $table->float('longitude', 13, 10);
+            $table->integer('zoom');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+}

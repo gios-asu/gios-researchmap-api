662fd34859d6fd1bb79449f29b68600582558d11
Nathan Rollins <nathan.rollins@asu.edu>
2018-01-31 11:22:56 -0700

    Commit Location and Project models

---
+++ app/Location.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Location extends Model
+{
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id',
+        'name',
+        'size',
+        'latitude',
+        'longitude',
+        'zoom',
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+}
